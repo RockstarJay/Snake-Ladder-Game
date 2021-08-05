d3f3e85b50fc69b2f7e62e814ef9b6358f1b7aa3
unknown <RockstarJay>
2021-08-05 13:37:07 +0530

    Added: welcome to the snake and ladder

---
+++ src/com/jaybhagat/snakeladder/SnakeLadder.java
@@ -0,0 +1,13 @@
+package com.jaybhagat.snakeladder;
+
+/*
+ * class to print welcome message on the console
+ * @author com.jaybhagat
+ */
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Game.");
+	}
+}
