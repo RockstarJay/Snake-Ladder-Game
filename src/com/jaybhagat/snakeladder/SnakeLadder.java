e3c20be6a7afe837c0415a1cbf260dc93a20d096
unknown <RockstarJay>
2021-08-05 13:50:52 +0530

    Modified: using random to get dice roll between 1 to 6

--- src/com/jaybhagat/snakeladder/SnakeLadder.java
+++ src/com/jaybhagat/snakeladder/SnakeLadder.java
@@ -2,14 +2,15 @@ package com.jaybhagat.snakeladder;
 
 /*
  * class to initialize the game with start position zero
+ * using random to dice roll to get a number from 1 to 6
  * @author com.jaybhagat
  */
 
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		int startPosition = 0;                                        // initialize startPosition to zero
-		System.out.println("Game is for Single Player");
-		System.out.println("Starting Position is: "+startPosition);
+		int startPosition = 0;                                           // initialize startPosition to zero
+		int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);    // it will generate 1 to 6 number randomly
+		System.out.println("Dice Roll Number : "+ dieRoll);
 	}
 }
